427e925d82720ab12c49ae8d0f751933c21cb9e2
Tingting Bao <baoting.tina@gmail.com>
2016-12-26 10:40:09 -0800

    pep8

--- blog.py
+++ blog.py
@@ -2,7 +2,7 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+    render_template, flash
 
 # create our little application
 app = Flask(__name__)
@@ -10,87 +10,103 @@ app.config.from_object(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-	SECRET_KEY='development key',
-	USERNAME='admin',
-	PASSWORD='default'
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
 def connect_db():
-	"""Connects to the specific database."""
-	rv = sqlite3.connect(app.config['DATABASE'])
-	rv.row_factory = sqlite3.Row
-	return rv
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
 
 def get_db():
-	"""Opens a new database connection if there is none yet for the 
-	current application context.
-	"""
-	if not hasattr(g, 'sqlite_db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
 
 @app.teardown_appcontext
 def close_db(error):
-	"""Close the databse again at the end of request."""
-	if hasattr(g, 'sqlite_db'):
-		g.sqlite_db.close()
+    """Close the databse again at the end of request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 def init_db():
-	db = get_db()
-	with app.open_resource('schema.sql', mode = 'r') as f:
-		db.cursor().executescript(f.read())
-	db.commit()
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
 
 @app.cli.command('initdb')
 def initdb_command():
-	"""Initializes the database."""
-	init_db()
-	print 'Initialized the databse.'
+    """Initializes the database."""
+    init_db()
+    print 'Initialized the databse.'
+
 
 @app.route('/')
 def show_entries():
-	db = get_db()
-	cur = db.execute('select id, title, text from entries order by id desc')
-	entries = cur.fetchall()
-	return render_template('show_entries.html', entries = entries)
+    db = get_db()
+    cur = db.execute('select id, title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
 
-@app.route('/write', methods = ['POST', 'GET'])
+
+@app.route('/admin')
+def administration():
+    pass
+
+
+@app.route('/write', methods=['POST', 'GET'])
 def add_entry():
-	if not session.get('logged_in'):
-		abort(401)
-	if request.method == 'POST':
-		db = get_db()
-		db.execute('insert into entries (title, text) values (?, ?)',
-			        [request.form['title'], request.form['text']])
-		db.commit()
-		flash('New entry was successfully posted')
-		return redirect(url_for('show_entries'))
-	return render_template('write_entry.html')
+    if not session.get('logged_in'):
+        abort(401)
+    if request.method == 'POST':
+        db = get_db()
+        db.execute('insert into entries (title, text) values (?, ?)',
+                   [request.form['title'], request.form['text']])
+        db.commit()
+        flash('New entry was successfully posted')
+        return redirect(url_for('show_entries'))
+    return render_template('write_entry.html')
 
-@app.route('/view/<int:post_id>', methods = ['GET'])
+
+@app.route('/view/<int:post_id>', methods=['GET'])
 def view_entry(post_id):
-	db = get_db()
-	cur = db.execute('select title, text from entries where id=(?)', [post_id])
-	entry = cur.fetchone()
-	return render_template('show_entry.html', entry=entry)
+    db = get_db()
+    cur = db.execute('select title, text from entries where id=(?)', [post_id])
+    entry = cur.fetchone()
+    return render_template('show_entry.html', entry=entry)
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None 
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
-			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid password'
-		else:
-			session['logged_in'] = True
-			flash('You were logged in')
-			return redirect(url_for('show_entries'))
-	return render_template('login.html', error = error)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('show_entries'))
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
